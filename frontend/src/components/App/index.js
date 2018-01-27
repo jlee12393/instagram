efcc46ead44d70a14ff56ea22bfe0f5332e807fd
jlee <jlee12393@gmail.com>
2018-01-27 19:51:29 +0900

    implementing frontend

---
+++ frontend/src/components/App/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import styles from './style.scss';
+
+class App extends Component {
+    render() {
+        return <div className={styles.App} />;
+    }
+}
+
+export default App;
---
+++ frontend/src/translations.js
@@ -0,0 +1,5 @@
+export const translations = {
+    kr: {
+        "Log in": "안녕하세요"
+    }
+}
