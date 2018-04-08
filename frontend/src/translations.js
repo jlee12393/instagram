a5a78ac61519aef7050c0044dbf5064b0cd28ace
jlee <jlee12393@gmail.com>
2018-04-08 13:45:14 +0900

    kr language version added

--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,5 +1,19 @@
+
 export const translations = {
     kr: {
-        "Log in": "안녕하세요"
+        // "About Us": "어바웃 어스",
+        "Log in": "로그인",
+        "Password": "비밀번호",
+        "Username": "아이디",
+        "UserName": "아이디",
+        "Log in with Facebook" : "페이스북 로그인",
+        "Forgot password?" : "비밀번호 찾기",
+        "Email": "이메일",
+        "Full Name": "성명",
+        "Sign up" : "가입하기",
+        "Don't have an account?": "계정이 없으신가요?",
+        "Have an account?": "가입하셨나요?",
+        "Get the app": "어플 다운 받기",
+
     }
-}
+};
